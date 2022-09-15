361cbfa0ebd1725d3d6eaa519eaa7a133c9fccbe
lucasajajpiccoli <lucasajaj@gmail.com>
2022-09-15 13:39:54 -0300

    Definição dos status codes e criação das rotas de autenticação

--- src/app.js
+++ src/app.js
@@ -7,9 +7,13 @@ import { v4 as uuid } from 'uuid';
 import './setup.js';
 import { db } from './database/db.js';
 
+import { authRouter } from './routes/authRouter.js';
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(authRouter);
 
 export {
     app
---
+++ src/routes/authRouter.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+const authRouter = Router();
+
+// authRouter.post('/login', _);
+
+// authRouter.post('/signup', _);
+
+export {
+    authRouter
+};
