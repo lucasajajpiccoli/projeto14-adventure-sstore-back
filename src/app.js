24c7aae53fe6c05cd4745415ea367dac4e5ddb61
lucasajajpiccoli <lucasajaj@gmail.com>
2022-09-19 11:45:25 -0300

    Terminadas as funcionalidades das rotas de produtos

--- src/app.js
+++ src/app.js
@@ -4,12 +4,14 @@ import cors from 'cors';
 import { v4 as uuid } from 'uuid';
 
 import { authRouter } from './routes/authRouter.js';
+import { productsRouter } from './routes/productsRouter.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use(authRouter);
+app.use(productsRouter);
 
 export {
     app
